949f48b0490d4fee9b20454b189a9fe52db075b0
Shawn Pais <pais.the.designer@gmail.com>
2021-10-11 22:18:39 -0400

    initial setup complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+    </div>
+  );
+};
+
+export default App;
