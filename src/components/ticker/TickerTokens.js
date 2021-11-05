56214f44d38f6b0a086e86cd5722c529ca948851
Shawn Pais <pais.the.designer@gmail.com>
2021-11-04 22:51:38 -0400

    moved requests to child

--- src/components/ticker/TickerTokens.js
+++ src/components/ticker/TickerTokens.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "../../utilities/axios";
+import requests from "../../utilities/requests";
 
 const TickerTokens = ({ fetchURL }) => {
   const [tickerData, setTickerData] = useState(null);
@@ -9,54 +10,50 @@ const TickerTokens = ({ fetchURL }) => {
     return (num = round);
   };
 
-  async function requestData() {
-    const res = await axios.get(fetchURL);
-    //console.log(res);
-    setTickerData(res.data);
-  }
-
   useEffect(() => {
+    const requestData = async () => {
+      const res = await axios.get(requests.fetchCurrenciesTicker);
+      setTickerData(res.data);
+    };
     requestData();
-    return () => res;
+    return () => requestData;
   }, [fetchURL]);
 
   return (
-    <div className="ticker__element">
-      <div className="header-coins">
-        {tickerData &&
-          tickerData
-            .filter((token) => token.rank <= 5) // Display top 20 tokens
-            .map((token, tokenIndex) => {
-              return (
-                <div className="header-coin" key={tokenIndex}>
-                  <div className="header-coin__img">
-                    <img src={token.logo_url} alt={token.name} />
+    <div className="header-coins">
+      {tickerData &&
+        tickerData
+          .filter((token) => token.rank <= 5) // Display top 20 tokens
+          .map((token, tokenIndex) => {
+            return (
+              <div className="header-coin" key={tokenIndex}>
+                <div className="header-coin__img">
+                  <img src={token.logo_url} alt={token.name} />
+                </div>
+                <div className="header-coin__content-body">
+                  <div className="header-coin__content-text">
+                    <span className="header-coin__content-price">
+                      ${roundDecimal(token.price)}
+                    </span>
+                    <span className="header-coin__content-name">
+                      {token.currency}
+                      <span className="header-coin__content-ranking">
+                        {token.rank}
+                      </span>
+                    </span>
                   </div>
-                  <div className="header-coin__content-body">
-                    <div className="header-coin__content-text">
-                      <span className="header-coin__content-price">
-                        ${roundDecimal(token.price)}
-                      </span>
-                      <span className="header-coin__content-name">
-                        {token.currency}
-                        <span className="header-coin__content-ranking">
-                          {token.rank}
-                        </span>
-                      </span>
-                    </div>
-                    <div className="header-coin__content-text">
-                      <span className="header-coin__content-percentage up">
-                        {roundDecimal(token["1d"].price_change)}%
-                      </span>
-                      <span className="header-coin__content-percentage gray-text">
-                        24HRs
-                      </span>
-                    </div>
+                  <div className="header-coin__content-text">
+                    <span className="header-coin__content-percentage up">
+                      {roundDecimal(token["1d"].price_change)}%
+                    </span>
+                    <span className="header-coin__content-percentage gray-text">
+                      24HRs
+                    </span>
                   </div>
                 </div>
-              );
-            })}
-      </div>
+              </div>
+            );
+          })}
     </div>
   );
 };
