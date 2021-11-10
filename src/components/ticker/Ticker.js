f7af26779e52a421ab10577c22b64d7b5672dab2
Shawn Pais <pais.the.designer@gmail.com>
2021-11-09 22:24:54 -0500

    updated call function and child

--- src/components/ticker/Ticker.js
+++ src/components/ticker/Ticker.js
@@ -1,15 +1,46 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "../../utilities/axios";
 import requests from "../../utilities/requests";
 import TickerTokens from "./TickerTokens";
 import "./ticker.styles.scss";
 
 const Ticker = () => {
+  const [tickerData, setTickerData] = useState(null);
+
   useEffect(() => {
-    console.log("component did mount");
+    const requestData = async () => {
+      const res = await axios.get(requests.fetchCurrenciesTicker);
+      setTickerData(
+        res.data.filter((token) => {
+          return token.rank <= 5;
+        })
+      );
+    };
+
+    requestData();
+    return () => requestData;
   }, []);
+
   return (
     <div className="ticker">
-      <TickerTokens fetchURL={requests.fetchCurrenciesTicker} />
+      <div className="ticker__element">
+        <div className="header-coins">
+          {tickerData &&
+            tickerData.map((token, tokenIndex) => {
+              return (
+                <TickerTokens
+                  key={tokenIndex}
+                  logo={token.logo_url}
+                  name={token.name}
+                  price={token.price}
+                  currency={token.currency}
+                  rank={token.rank}
+                  priceChange={token["1d"].price_change}
+                />
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ticker/TickerTokens.js
+++ src/components/ticker/TickerTokens.js
@@ -1,64 +1,35 @@
-import { useEffect, useState } from "react";
-import axios from "../../utilities/axios";
-import requests from "../../utilities/requests";
-
-const TickerTokens = () => {
-  const [tickerData, setTickerData] = useState(null);
-
+function TickerTokens(props) {
   const roundDecimal = (num) => {
     let round = Number.parseFloat(num).toFixed(2);
     return (num = round);
   };
 
-  useEffect(() => {
-    const requestData = async () => {
-      const res = await axios.get(requests.fetchCurrenciesTicker);
-      setTickerData(
-        res.data.filter((token) => {
-          return token.rank <= 5;
-        })
-      );
-    };
-
-    requestData();
-    return () => requestData;
-  }, []);
-
   return (
-    <div className="header-coins">
-      {tickerData &&
-        tickerData.map((token, tokenIndex) => {
-          return (
-            <div className="header-coin" key={tokenIndex}>
-              <div className="header-coin__img">
-                <img src={token.logo_url} alt={token.name} />
-              </div>
-              <div className="header-coin__content-body">
-                <div className="header-coin__content-text">
-                  <span className="header-coin__content-price">
-                    ${roundDecimal(token.price)}
-                  </span>
-                  <span className="header-coin__content-name">
-                    {token.currency}
-                    <span className="header-coin__content-ranking">
-                      {token.rank}
-                    </span>
-                  </span>
-                </div>
-                <div className="header-coin__content-text">
-                  <span className="header-coin__content-percentage up">
-                    {roundDecimal(token["1d"].price_change)}%
-                  </span>
-                  <span className="header-coin__content-percentage gray-text">
-                    24HRs
-                  </span>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+    <div className="header-coin">
+      <div className="header-coin__img">
+        <img src={props.logo} alt={props.name} />
+      </div>
+      <div className="header-coin__content-body">
+        <div className="header-coin__content-text">
+          <span className="header-coin__content-price">
+            ${roundDecimal(props.price)}
+          </span>
+          <span className="header-coin__content-name">
+            {props.currency}
+            <span className="header-coin__content-ranking">{props.rank}</span>
+          </span>
+        </div>
+        <div className="header-coin__content-text">
+          <span className="header-coin__content-percentage up">
+            {roundDecimal(props.priceChange)}%
+          </span>
+          <span className="header-coin__content-percentage gray-text">
+            24HRs
+          </span>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default TickerTokens;
