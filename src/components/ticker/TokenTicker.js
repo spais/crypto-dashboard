0ce18f4e5fa44ff37c102a74c9451087d6165f7a
Shawn Pais <pais.the.designer@gmail.com>
2021-12-04 00:25:02 -0500

    ticker animation

--- src/components/ticker/TokenTicker.js
+++ src/components/ticker/TokenTicker.js
@@ -1,11 +1,33 @@
+import { useState, useEffect, useRef } from "react";
 import { TokenDataLoader } from "./TokenDataLoader";
 import "./ticker.styles.scss";
 import { TokenInfo } from "./TokenInfo";
 
 function TokenTicker() {
+  const [position, setPosition] = useState(0);
+  const [speed, setSpeed] = useState(15);
+  const elementWidth = useRef();
+
+  useEffect(() => {
+    const animate = setInterval(() => {
+      setPosition((x) => x - 1);
+    }, speed);
+    console.log(position);
+    return () => clearInterval(animate);
+  }, [speed]);
+
   return (
     <div className="ticker">
-      <div className="ticker__element">
+      <div
+        className="ticker__element"
+        style={{
+          willChange: "transform",
+          position: "absolute",
+          left: `${position}px`,
+          top: 0,
+        }}
+        ref={elementWidth}
+      >
         <div className="header-coins">
           <TokenDataLoader>
             <TokenInfo />
